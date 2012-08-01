395e20729886a471719ecac49949150529b3dbe9
Nicholas Tau <demon42111915@gmail.com>
2012-08-01 15:04:06 +0800

    add memory utils

---
+++ QuartzProject/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  uicolor
+//
+//  Created by Nicholas Tau on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
++(void)printMemoryInfo;
+@end
